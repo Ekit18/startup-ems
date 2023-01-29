1cd4d90fa3daaf5a17e349cd85c8869bb6eaa0c2
evgen <evgenkitan@gmail.com>
2023-01-29 23:57:43 +0200

    roles and first step of auth

---
+++ server/src/roles/user-roles.model.ts
@@ -0,0 +1,25 @@
+import { Model, Table, Column, DataType, BelongsToMany, ForeignKey } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+import { Role } from "./roles.model";
+
+
+interface RoleCreationAttrs{
+    value:string;
+    description:string;
+
+}
+@Table({tableName:'user_roles',createdAt:false, updatedAt:false})
+export class UserRoles extends Model<UserRoles>{
+    @Column({type:DataType.INTEGER, unique:true, autoIncrement:true, primaryKey:true})
+    id: number;
+
+
+    @ForeignKey(()=>User)
+    @Column({type:DataType.INTEGER})
+    userId: number;
+
+
+    @ForeignKey(()=>Role)
+    @Column({type:DataType.INTEGER})
+    roleId: number;
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { User } from './users.model';
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
+import { RolesService } from 'src/roles/roles.service';
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User) private userRepository:typeof User){}
+    constructor(@InjectModel(User) private userRepository:typeof User,
+    private roleService:RolesService){}
     async createUser(dto:CreateUserDto){ 
          const user= await this.userRepository.create(dto);
+         const role= await this.roleService.getRoleByValue("USER")
+         await user.$set('roles', [role.id])
          return user;
     }
 
