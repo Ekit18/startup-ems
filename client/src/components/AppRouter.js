8efac2463dd617b8747c3cbbcb43376dd7b08b51
Fedasevich <yurafedasevka@gmail.com>
2023-01-29 18:11:00 +0100

    front end init

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,30 @@
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { Context } from '../index';
+import { authRoutes, publicRoutes } from '../routes';
+import { LOGIN_ROUTE} from '../utils/constants';
+
+const AppRouter = observer(() => {
+    const {user} = useContext(Context)
+
+    console.log(user)
+  return (
+  
+    <Routes>
+        {user.isAuth && authRoutes.map(({path, Component}) =>
+        <Route key={path} path={path} element={<Component/>} exact/>
+        )}
+         {publicRoutes.map(({path, Component}) =>
+        <Route key={path} path={path} element={<Component/>} exact/>
+        )}
+         <Route
+        path="*"
+        element={<Navigate to={LOGIN_ROUTE} replace />}
+    />
+    </Routes>
+   
+  )
+})
+
+export default AppRouter
