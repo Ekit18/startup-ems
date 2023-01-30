4d0476ffb04b3d760b73b102ab3342cdf4f2df6c
evgen <evgenkitan@gmail.com>
2023-01-30 13:45:21 +0200

    auth guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,19 +1,18 @@
-import { Controller,Body, Post,Get } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
 import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
-import {CreateUserDto} from "./dto/create-user.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
 
-    constructor (private usersService: UsersService){}
+    constructor(private usersService: UsersService) { }
 
     @Post()
-    create(@Body() userDto:CreateUserDto){
+    create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
-
     @Get('/:value')
-    getByValue(@Param('value') value:string){
+    getByValue(@Param('value') value: string) {
         return this.usersService.getUserByEmail(value);
     }
 }
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -6,14 +6,17 @@ import { User } from './users.model';
 import { Role } from 'src/roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports:[
-    SequelizeModule.forFeature([User,Role,UserRoles]),
-    RolesModule
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule,
+    forwardRef(()=> AuthModule)
   ],
-  exports:[UsersService]
+  exports: [UsersService
+    ]
 })
-export class UsersModule {}
+export class UsersModule { }
