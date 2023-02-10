499995027dbae66b587c489a4b872e4b3a6343c6
Fedasevich <yurafedasevka@gmail.com>
2023-02-10 14:10:34 +0100

    fixed pipe bug

--- server/src/brand/brand.controller.ts
+++ server/src/brand/brand.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandDto } from './dto/brand.dto';
+
 @Controller('brand')
 export class BrandController {
     constructor(private brandService: BrandService) { }
@@ -8,21 +9,20 @@ export class BrandController {
     create(@Body() brandDto: BrandDto) {
         return this.brandService.createBrand(brandDto);
     }
-    @Get('/:value')
-    getByValue(@Param('value') value: string) {
-        console.log(value)
-        return this.brandService.getBrandByValue(value);
+    @Get('/:brand')
+    getByValue(@Param() params: BrandDto) {
+        return this.brandService.getBrandByValue(params);
     }
     @Get()
     getAll() {
         return this.brandService.getAllBrands();
     }
     @Put(':id')
-    update(@Param('id')id:number, @Body()brandDto:BrandDto){
-        return this.brandService.updateBrand(id,brandDto);
+    update(@Param() id: number, @Body() brandDto: BrandDto) {
+        return this.brandService.updateBrand(id, brandDto);
     }
     @Delete(':id')
-    remove(@Param('id')id:number){
+    remove(@Param() id: number) {
         return this.brandService.remove(id);
     }
 }
--- server/src/brand/brand.service.ts
+++ server/src/brand/brand.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Brand } from './brand.model';
 import { BrandDto } from './dto/brand.dto';
+
 @Injectable()
 export class BrandService {
-    constructor(@InjectModel(Brand) private brandRepository: typeof Brand){}
-    async createBrand(dto:BrandDto){
+    constructor(@InjectModel(Brand) private brandRepository: typeof Brand) { }
+    async createBrand(dto: BrandDto) {
         const brand = await this.brandRepository.create(dto);
         return brand;
     }
-    async getBrandByValue(value:string){
-        const brand = await this.brandRepository.findOne({where:{brand:value}, include:{all:true}});
+    async getBrandByValue(dto: BrandDto) {
+        const brand = await this.brandRepository.findOne({ where: { brand: dto.brand } });
+        if (!brand) {
+            throw new HttpException({message:'Wrong data'}, HttpStatus.BAD_REQUEST);
+        }
         return brand;
     }
-    async getAllBrands(){
-        const brands = await this.brandRepository.findAll({include: {all: true}});
+    async getAllBrands() {
+        const brands = await this.brandRepository.findAll({ include: { all: true } });
         return brands;
     }
-    async updateBrand(id:number, brandDto:BrandDto){
-       return Brand.update({brand:brandDto.brand},{where:{id:id}});
+    async updateBrand(id: number, brandDto: BrandDto) {
+        return Brand.update({ brand: brandDto.brand }, { where: { id: id } });
     }
-    async remove(id:number){
-       return Brand.destroy({where:{id}});
+    async remove(id: number) {
+        return Brand.destroy({ where: { id } });
     }
 }
