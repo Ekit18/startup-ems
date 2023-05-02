07c7d0598657095bb2b406411a3558f92b9afa2d
Fedasevich <yurafedasevka@gmail.com>
2023-05-02 11:13:56 +0200

    added service map icon

--- client/src/ui/icons.tsx
+++ client/src/ui/icons.tsx
@@ -1,9 +1,29 @@
 
 import { ReactComponent as Google } from './google.svg'
-
+import Service from './service.svg'
+import ReactDOMServer from 'react-dom/server';
+import L from 'leaflet';
 
 export const GoogleIcon = () => {
     return (
         <Google />
     )
 }
+
+
+export const CrashIcon = L.icon({
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+    iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png",
+});
+
+
+export const ServiceIcon = L.icon({
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+    iconUrl: Service,
+    shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png",
+});
